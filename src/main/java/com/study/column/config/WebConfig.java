455bff3a05755003d61e6eddda1c2da8f8c6431c
pinwormmy <ozkhan@naver.com>
2023-08-18 18:53:48 +0900

    전페이지에서 방문자수표시위해 인터셉터 사용
    페이지 이동시 방문자수 표시 안되는 버그 수정하려고 인터셉터사용
    
    - 발매 일정정보 구현하기(보류)
    - 메뉴 구성하기

--- src/main/java/com/study/column/config/WebConfig.java
+++ src/main/java/com/study/column/config/WebConfig.java
@@ -1,7 +1,10 @@
 package com.study.column.config;
 
 import com.study.column.interceptor.AdminInterceptor;
+import com.study.column.interceptor.VisitorCountInterceptor;
+import com.study.column.visitorCount.VisitorCountService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,6 +14,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @Slf4j
 public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    VisitorCountService visitorCountService;
 
     @Value("/img/**")
     private String connectPath;
@@ -29,7 +35,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new VisitorCountInterceptor(visitorCountService))
+                .addPathPatterns("/**");
         registry.addInterceptor(new AdminInterceptor())
                 .addPathPatterns("/**/writePost/**", "/**/modifyPost/**");
     }
+
 }
