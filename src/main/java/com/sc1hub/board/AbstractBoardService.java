b7dfec7560c9aa4e508f9f6625ff092225433084
pinwormmy <ozkhan@naver.com>
2023-09-10 01:37:08 +0900

    공통 로직 분리 작업 중-서비스 추상클래스 생성
    서비스인터페이스에서 잇는 추상클래스 생성
    이걸 각 게시판 서비스 구현 클래스랑 연결할 예정..
    그 다음 공용 dto 파일 만들기 등등...
    ㄴ공용 DTO 어떻게 구성할지 알아보기
    
    - 메인화면 명령어 기능 구현하기

---
+++ src/main/java/com/sc1hub/board/AbstractBoardService.java
@@ -0,0 +1,99 @@
+package com.sc1hub.board;
+
+import com.sc1hub.util.PageDTO;
+import com.sc1hub.util.PageService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public abstract class AbstractBoardService<T, R, C> implements BoardService<T, R, C> {
+
+    @Autowired
+    protected BoardMapper<T, R, C> boardMapper;
+
+    @Override
+    public List<T> showPostList(PageDTO page) throws Exception {
+        return boardMapper.showPostList(page);
+    }
+
+    @Override
+    public void submitPost(T board) throws Exception {
+        boardMapper.submitPost(board);
+    }
+
+    @Override
+    public T readPost(int postNum) throws Exception {
+        return boardMapper.readPost(postNum);
+    }
+
+    @Override
+    public void submitModifyPost(T post) throws Exception {
+        boardMapper.submitModifyPost(post);
+    }
+
+    @Override
+    public void deletePost(int postNum) throws Exception {
+        boardMapper.deletePost(postNum);
+    }
+
+    @Override
+    public PageDTO pageSetting(PageDTO page) throws Exception {
+        checkPageAndKeyword(page);
+        return utilLoadingForPage(page);
+    }
+
+    @Override
+    public void addComment(C comment) throws Exception {
+        boardMapper.addComment(comment);
+    }
+
+    @Override
+    public List<C> showCommentList(PageDTO page) throws Exception {
+        return boardMapper.showCommentList(page);
+    }
+
+    @Override
+    public void deleteComment(int commentNum) throws Exception {
+        boardMapper.deleteComment(commentNum);
+    }
+
+    @Override
+    public void updateCommentCount(int postNum) throws Exception {
+        boardMapper.updateCommentCount(postNum);
+    }
+
+    @Override
+    public void updateViews(int postNum) throws Exception {
+        boardMapper.updateViews(postNum);
+    }
+
+    @Override
+    public int checkViewUserIp(int postNum, String ip) throws Exception {
+        return boardMapper.checkViewUserIp(postNum, ip);
+    }
+
+    @Override
+    public void saveViewUserIp(int postNum, String ip) throws Exception {
+        boardMapper.saveViewUserIp(postNum, ip);
+    }
+
+    @Override
+    public List<T> showSelfNoticeList() throws Exception {
+        return boardMapper.showSelfNoticeList();
+    }
+
+    protected void checkPageAndKeyword(PageDTO page) {
+        if(page.getRecentPage() < 1) { page.setRecentPage(1); }
+        if(page.getSearchType() == null) { page.setSearchType("title"); }
+        if(page.getKeyword() == null) { page.setKeyword(""); }
+    }
+
+    protected PageDTO utilLoadingForPage(PageDTO page) throws Exception {
+        page.setTotalPostCount(countTotalPost(page));
+        PageService util = initPageUtil();
+        return util.calculatePage(page);
+    }
+
+    protected abstract PageService initPageUtil();
+}
+
