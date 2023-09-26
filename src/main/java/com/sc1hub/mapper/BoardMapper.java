45e061c7ce1001d626ce7ddab0ef5abf88fc3c64
pinwormmy <ozkhan@naver.com>
2023-09-27 01:08:29 +0900

    컨트롤러에서 매퍼까지 모듈화해서 테스트 중. 오류 연이어 속출
    변수명 관련 오류가 대부분. 테스트하며 찾고 바로 잡기
    
    - 메인화면 명령어 기능 구현하기

--- src/main/java/com/sc1hub/mapper/BoardMapper.java
+++ src/main/java/com/sc1hub/mapper/BoardMapper.java
@@ -14,25 +14,25 @@ public interface BoardMapper {
 
     List<BoardDTO> showPostList(@Param("boardTitle") String boardTitle, @Param("page") PageDTO page) throws Exception;
 
-    void submitPost(@Param("boardTitle") String boardTitle, BoardDTO board) throws Exception;
+    void submitPost(@Param("boardTitle") String boardTitle, @Param("board") BoardDTO board) throws Exception;
 
-    void submitModifyPost(@Param("boardTitle") String boardTitle, BoardDTO post) throws Exception;
+    void submitModifyPost(@Param("boardTitle") String boardTitle, @Param("board") BoardDTO board) throws Exception;
 
-    BoardDTO readPost(@Param("boardTitle") String boardTitle, int postNum) throws Exception;
+    BoardDTO readPost(@Param("boardTitle") String boardTitle, @Param("postNum") int postNum) throws Exception;
 
-    void deletePost(@Param("boardTitle") String boardTitle, int postNum) throws Exception;
+    void deletePost(@Param("boardTitle") String boardTitle, @Param("postNum") int postNum) throws Exception;
 
-    int countTotalPost(@Param("boardTitle") String boardTitle, PageDTO page) throws Exception;
+    int countTotalPost(@Param("boardTitle") String boardTitle, @Param("page") PageDTO page) throws Exception;
 
-    void addComment(@Param("boardTitle") String boardTitle, CommentDTO comment) throws Exception;
+    void addComment(@Param("boardTitle") String boardTitle, @Param("comment") CommentDTO comment) throws Exception;
 
-    List<CommentDTO> showCommentList(@Param("boardTitle") String boardTitle, PageDTO page) throws Exception;
+    List<CommentDTO> showCommentList(@Param("boardTitle") String boardTitle, @Param("page") PageDTO page) throws Exception;
 
-    void deleteComment(@Param("boardTitle") String boardTitle, int commentNum) throws Exception;
+    void deleteComment(@Param("boardTitle") String boardTitle, @Param("commentNum") int commentNum) throws Exception;
 
-    void updateCommentCount(@Param("boardTitle") String boardTitle, int postNum) throws Exception;
+    void updateCommentCount(@Param("boardTitle") String boardTitle, @Param("postNum") int postNum) throws Exception;
 
-    void updateViews(@Param("boardTitle") String boardTitle, int postNum) throws Exception;
+    void updateViews(@Param("boardTitle") String boardTitle, @Param("postNum") int postNum) throws Exception;
 
     int checkViewUserIp(@Param("boardTitle") String boardTitle, @Param("postNum") int postNum, @Param("ip") String ip) throws Exception;
 
@@ -40,17 +40,18 @@ public interface BoardMapper {
 
     List<BoardDTO> showSelfNoticeList(@Param("boardTitle") String boardTitle) throws Exception;
 
-    int countTotalComment(@Param("boardTitle") String boardTitle, PageDTO page) throws Exception;
+    int countTotalComment(@Param("boardTitle") String boardTitle, @Param("page") PageDTO page) throws Exception;
 
-    int checkRecommendation(@Param("boardTitle") String boardTitle, RecommendDTO recommendDTO);
+    int checkRecommendation(@Param("boardTitle") String boardTitle, @Param("recommendDTO") RecommendDTO recommendDTO);
 
-    void insertRecommendation(@Param("boardTitle") String boardTitle, RecommendDTO recommendDTO);
+    void insertRecommendation(@Param("boardTitle") String boardTitle, @Param("recommendDTO") RecommendDTO recommendDTO);
 
-    void deleteRecommendation(@Param("boardTitle") String boardTitle, RecommendDTO recommendDTO);
+    void deleteRecommendation(@Param("boardTitle") String boardTitle, @Param("recommendDTO") RecommendDTO recommendDTO);
 
-    void updateTotalRecommendCount(@Param("boardTitle") String boardTitle, int postNum);
+    void updateTotalRecommendCount(@Param("boardTitle") String boardTitle, @Param("postNum") int postNum);
 
-    int getRecommendCount(@Param("boardTitle") String boardTitle, int postNum);
+    int getRecommendCount(@Param("boardTitle") String boardTitle, @Param("postNum") int postNum);
 
-    int getActualRecommendCount(@Param("boardTitle") String boardTitle, int postNum);
+    int getActualRecommendCount(@Param("boardTitle") String boardTitle, @Param("postNum") int postNum);
 }
+
